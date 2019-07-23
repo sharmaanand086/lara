e2a17b532ae3bcfd9163624017f38d2671279192
sharmaanand086 <38725515+sharmaanand086@users.noreply.github.com>
2019-07-23 17:38:29 +0530

    Update thankyou.blade.php

--- resources/views/layouts/thankyou.blade.php
+++ resources/views/layouts/thankyou.blade.php
@@ -5,7 +5,7 @@
 <div class="header" style="">
   <div class="inner-header thank" style="">
     <div class="logo">
-      <img src="http://localhost/belief/resources/assets/img/logo.png">
+      <img src="http://magicconversion.com/belief/resources/assets/img/logo.png">
     </div>
     <p style="">username</p>
   </div>
@@ -18,6 +18,29 @@
 
           @endforeach 
 	</div>
+
 </div>
+	<div class="container">
+		<div  class="formclass">
+		  @foreach ($q as $key => $questions)
+		 <p>{{  $questions }}</p>
+         
+          @endforeach 
+	</div>
+	
+	</div>
 
 @endsection
+
+
+
+ <style>
+        .formclass{
+   width: 70%;   
+   margin: auto;  
+   font-size: 32px;
+    color: #000;
+    justify-content: center;
+    flex-direction: column;
+        }
+    </style>
